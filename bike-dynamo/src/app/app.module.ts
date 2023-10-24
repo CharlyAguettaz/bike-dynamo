a4e6a8c2e06b6937ad0fa39fc3ffbcac2b25a085
Charly Aguettaz <caguettaztaz@gmail.com>
2023-10-24 23:49:23 +0200

    dashboard

--- bike-dynamo/src/app/app.module.ts
+++ bike-dynamo/src/app/app.module.ts
@@ -6,11 +6,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BatteryStatus } from '@ionic-native/battery-status/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [BatteryStatus, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- bike-dynamo/src/app/dashboard/dashboard.page.ts
+++ bike-dynamo/src/app/dashboard/dashboard.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Chart, registerables, Plugin } from 'chart.js';
-import PluginService from 'chart.js/dist/core/core.plugins';
+import { BatteryInfo, Device } from '@capacitor/device';
+import { Chart, registerables } from 'chart.js';
 Chart.register(...registerables);
 
 @Component({
@@ -10,16 +10,63 @@ Chart.register(...registerables);
 })
 export class Dashboard implements OnInit {
 
-  public chart: any;
+  public chart: Chart<"doughnut", string[], string>;
   public remainingTime: any;
-  public battery: any;
+  public battery: BatteryInfo = {
+    batteryLevel: 0,
+    isCharging: false
+  };
+  public badges: {
+    title: string;
+    url: string;
+    unlocked: boolean;
+  }[] = [
+    { title: 'phone', url: 'assets/badges/phone.svg', unlocked: true },
+    { title: 'phone5', url: 'assets/badges/phone5.svg', unlocked: true },
+    { title: 'phone10', url: 'assets/badges/phone10.svg', unlocked: false },
+    { title: 'laptop5', url: 'assets/badges/laptop5.svg', unlocked: true },
+    { title: 'laptop10', url: 'assets/badges/laptop10.svg', unlocked: false },
+    { title: 'laptop20', url: 'assets/badges/laptop20.svg', unlocked: false },
+    { title: 'car', url: 'assets/badges/car.svg', unlocked: false },
+  ];
+
+  public stickers: {
+    title: string;
+    url: string;
+    canSendTo: string;
+  }[] = [
+    { title: 'beat-you', url: 'assets/stickers/beat-you.png', canSendTo: 'all' },
+    { title: 'catch-you', url: 'assets/stickers/catch-you.png', canSendTo: 'all' },
+    { title: 'review-mirror', url: 'assets/stickers/review-mirror.png', canSendTo: 'all' },
+  ]
 
   constructor() {}
 
   ngOnInit(): void {
-    this.createChart();
-    this.remainingTime = '1h';
-    this.battery = '20%';
+    this.remainingTime = '0';
+    this.setBattery();
+    setInterval(() => this.updateBattery(), 1000);
+  }
+
+  async setBattery() {
+    await Device.getBatteryInfo().then((info) => {
+      this.battery = info;
+      if (this.battery.batteryLevel && this.battery.isCharging) this.remainingTime = (3500 * (1 - this.battery.batteryLevel) / 600).toPrecision(2);
+      this.createChart();
+    })
+  }
+
+  async updateBattery() {
+    await Device.getBatteryInfo().then((info) => {
+      this.battery.isCharging = info.isCharging;
+      if (info.batteryLevel !== this.battery.batteryLevel) {
+        this.battery = info;
+        if (this.battery.batteryLevel) this.chart.data.datasets[0].data = [(this.battery.batteryLevel * 100).toFixed(), (100 - this.battery.batteryLevel * 100).toFixed()];
+          this.chart.update();
+      }
+
+      if (this.battery.batteryLevel) this.remainingTime = (3500 * (1 - this.battery.batteryLevel) / 600).toPrecision(2);
+    })
   }
 
   createChart(){
@@ -33,17 +80,20 @@ export class Dashboard implements OnInit {
         const { ctx, data } = chart;
         ctx.save();
 
-        const text = data.labels[data.labels.length];
+        const text = data.datasets[0].data[0] + '%';
 
         const x = chart.getDatasetMeta(0).data[0].x;
-        const y = chart.getDatasetMeta(0).data[0].y;
+        const y = chart.getDatasetMeta(0).data[0].y + 4;
 
-        ctx.fillRect(x - 10, y - 10, 20);
+        ctx.textAlign = 'center';
+        ctx.textABaseline = 'middle';
+        ctx.font = 'bold 16px sans-serif';
         ctx.fillText(text, x, y);
+        ctx.fillStyle = 'white';
       }
     }
 
-    if(ctx) {
+    if(ctx && this.battery.batteryLevel) {
       this.chart = new Chart(ctx, {
         type: 'doughnut', //this denotes tha type of chart
 
@@ -51,7 +101,7 @@ export class Dashboard implements OnInit {
           labels: ['Load', 'Remaining time'],
            datasets: [
             {
-              data: ['20','80'],
+              data: [ (this.battery.batteryLevel * 100).toFixed(), (100 - this.battery.batteryLevel * 100).toFixed()],
               backgroundColor: ['#22BB88', '#22BB8850'],
               borderRadius: [3, 0],
               borderAlign: 'center',
